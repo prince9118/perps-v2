a1953608c54f92e2fe42f648a12d37e616397b31
prince9118 <pchaudharyskn@gmail.com>
2026-06-13 18:27:17 +0530

    webSocket setup

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,17 +1,47 @@
 import { WebSocketServer } from "ws";
-import {redis}from "@repo/redis";
-const wss=new WebSocketServer({
-    port:8080,
-});
+import { redis } from "@repo/redis";
 
-wss.on("connection",(socket)=>{
-    console.log("client Connected");
-    socket.send(JSON.stringify({
-        type:"CONNECTED",
-        message:"Websocket Connected",
-    }));
-    socket.on("message",(message)=>{
-        console.log("Received:",message.toString());
-    });
+const wss = new WebSocketServer({ port: 8080 });
+wss.on("connection", (socket) => {
+  console.log("Client connected");
+
+  socket.send(JSON.stringify({
+    type: "CONNECTED",
+    message: "WebSocket connected",
+  }));
 });
-console.log("WebScoket server running on port 8080");
+async function listenTradeEvents() {
+  let lastId = "$";
+  while (true) {
+    const result = await redis.xread(
+      "BLOCK",
+      0,
+      "STREAMS",
+      "trade_events",
+      lastId
+    );
+
+    if (!result) continue;
+
+    const [, messages] = result[0]!;
+
+    for (const [messageId, fields] of messages) {
+      const dataIndex = fields.indexOf("data");
+      const trade = JSON.parse(fields[dataIndex + 1]!);
+
+      for (const client of wss.clients) {
+        if (client.readyState === 1) {
+          client.send(JSON.stringify({
+            type: "TRADE_CREATED",
+            data: trade,
+          }));
+        }
+      }
+      lastId = messageId;
+    }
+  }
+}
+
+listenTradeEvents();
+
+console.log("WebSocket server running on port 8080");
